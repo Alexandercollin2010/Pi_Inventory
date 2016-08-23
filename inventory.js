f5666fbfede47844d8a84de75216d5024a18557b
devjanaprime <dev@primeacademy.io>
2016-08-23 10:47:21 -0500

    first

---
+++ inventory.js
@@ -0,0 +1,6 @@
+console.log( 'inventory.js sourced' );
+
+var getSizeSelection = function(){
+  console.log( 'in getSizeSelection' );
+  console.log( 'selected size:', document.getElementById('sizeIn').value );
+} // end getSizeSelection
