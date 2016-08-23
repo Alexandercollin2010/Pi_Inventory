1586e0f547ecca38b8d7573967c658f9d55b1048
devjanaprime <dev@primeacademy.io>
2016-08-23 14:24:14 -0500

    lecture4

--- inventory.js
+++ inventory.js
@@ -1,14 +1,21 @@
 console.log( 'inventory.js sourced' );
 // global vars
-var inventory;
+var inventory=[];
 var selectedColor ='none';
 var selectedSize ='none';
 
 var addItem = function( color, name, size ){
   console.log( 'adding item: ' + name + ' ' + color + ' ' + size );
   // create object
+  var newItem = {
+    color: color,
+    name: name,
+    size: size
+  }; // end newItem
   // push into array
+  inventory.push( newItem );
   // show updated inventory
+  console.log( inventory );
 } // end addItem
 
 var searchInventory = function(){
@@ -19,10 +26,22 @@ var searchInventory = function(){
   selectedColor = document.getElementById( 'colorIn' ).value;
   console.log( 'looking for something ' + selectedColor + ' and ' + selectedSize );
   // check if an item fits this description
+  // loop through the inventory array and see if any item matches
+  // array for our results
+  var results = [];
+  for( var i = 0; i < inventory.length; i++ ){
+    // does this item match my description?
+    if( inventory[ i ].color == selectedColor && inventory[ i ].size == selectedSize ){
+      results.push( inventory[ i ] );
+    }
+  }; // end for
+  console.log( results.length + ' matches found' );
+  console.log( results );
 } // end searchInventory
 
 ///////// ADD ITEMS /////////
 addItem( 'blue', 'Smurf', 'Small');
+addItem( 'blue', 'blueberry', 'Small');
 addItem( 'mermaid treasure', 'Prime Academy', 'Large');
 addItem( 'bus seat green', 'Baby Banner', 'Medium');
 addItem( 'purple', 'Eggplant', 'Small');
